b2eb08140cb1a0e130aaa59a8fe6777a668d1483
ChrisB007 <chrisbitoy@gmail.com>
2021-10-04 09:22:11 -0400

    removed server codes

--- pages/api/portfolios/[portfolioId].js
+++ pages/api/portfolios/[portfolioId].js
@@ -1,34 +1,34 @@
-import { portfolios } from "./projectArray";
+// import { portfolios } from "./projectArray";
 
-async function singlePortfolio(req, res) {
-  const portfolioId = req.query.portfolioId;
-  const { title, url, description, type, image } = req.body;
-  const httpMethod = req.method;
-  const result = portfolios.filter(
-    (portfolio) => portfolio.id === parseInt(portfolioId)
-  );
+// async function singlePortfolio(req, res) {
+//   const portfolioId = req.query.portfolioId;
+//   const { title, url, description, type, image } = req.body;
+//   const httpMethod = req.method;
+//   const result = portfolios.filter(
+//     (portfolio) => portfolio.id === parseInt(portfolioId)
+//   );
 
-  switch (httpMethod) {
-    case "GET":
-      if (result.length > 0) {
-        res.status(200).json(result[0]);
-      } else {
-        res
-          .status(404)
-          .json(`message: Portfolio with id ${portfolioId} is not found`);
-      }
-      break;
-    case "PUT":
-      res.status(200).json({ title, url, description, type, image });
-      break;
-    case "DELETE":
-      res.status(200).json(result[0]);
-      break;
-    default:
-      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
-      res.status(405).end(`Method ${httpMethod} not allowed`);
-      break;
-  }
-}
+//   switch (httpMethod) {
+//     case "GET":
+//       if (result.length > 0) {
+//         res.status(200).json(result[0]);
+//       } else {
+//         res
+//           .status(404)
+//           .json(`message: Portfolio with id ${portfolioId} is not found`);
+//       }
+//       break;
+//     case "PUT":
+//       res.status(200).json({ title, url, description, type, image });
+//       break;
+//     case "DELETE":
+//       res.status(200).json(result[0]);
+//       break;
+//     default:
+//       res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+//       res.status(405).end(`Method ${httpMethod} not allowed`);
+//       break;
+//   }
+// }
 
-export default singlePortfolio;
+// export default singlePortfolio;
--- pages/api/portfolios/index.js
+++ pages/api/portfolios/index.js
@@ -1,21 +1,21 @@
-import portfolios from "../../portfolio";
+// import portfolios from "../../portfolio";
 
-async function index(req, res) {
-  const httpMethod = req.method;
-  const { title, image, description, url, type } = req.body;
+// async function index(req, res) {
+//   const httpMethod = req.method;
+//   const { title, image, description, url, type } = req.body;
 
-  switch (httpMethod) {
-    case "GET":
-      await res.status(200).json(portfolios);
-      break;
-    case "POST":
-      res.status(200).json({ title, image, description, url, type });
-      break;
-    default:
-      res.setHeader("Allow", ["GET", "POST"]);
-      res.status(405).end(`Method ${httpMethod} not allowed`);
-      break;
-  }
-}
+//   switch (httpMethod) {
+//     case "GET":
+//       await res.status(200).json(portfolios);
+//       break;
+//     case "POST":
+//       res.status(200).json({ title, image, description, url, type });
+//       break;
+//     default:
+//       res.setHeader("Allow", ["GET", "POST"]);
+//       res.status(405).end(`Method ${httpMethod} not allowed`);
+//       break;
+//   }
+// }
 
-export default index;
+// export default index;
