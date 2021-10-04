1b4e65d5b28f4a7f137dcb8f0bdc920f7c5ce2fe
ChrisB007 <chrisbitoy@gmail.com>
2021-10-04 11:27:36 -0400

    database management updated

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -19,15 +19,7 @@ export default NextAuth({
   // Notes:
   // * You must install an appropriate node_module for your database
   // * The Email provider requires a database (OAuth providers do not)
-  database: {
-    type: "mongodb",
-    uri: process.env.DATABASE_URL,
-    WriteConcern: { w: "majority", wtimeout: 2500 },
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    retryWrites: true,
-    synchronize: true,
-  },
+  database: process.env.DATABASE_URL,
 
   // The secret should be set to a reasonably long random string.
   // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
